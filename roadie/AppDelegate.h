3d3301c33dbc0ed972812fde29be57a8448ad37e
Xin Suo <xsuo@groupon.com>
2015-11-18 09:49:04 -0800

    Initial Commit

---
+++ roadie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  roadie
+//
+//  Created by Xin Suo on 11/18/15.
+//  Copyright © 2015 roadie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ roadie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  roadie
+//
+//  Created by Xin Suo on 11/18/15.
+//  Copyright © 2015 roadie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
