34d0481d226513416f1b6739684c0b29d584b5f2
Robin Wu <rwu@groupon.com>
2015-11-27 23:07:04 -0800

    add HotelDetailController worked so far, feature not done yet.

---
+++ roadie/HotelDetailController.h
@@ -0,0 +1,13 @@
+//
+//  HotelDetailController.h
+//  roadie
+//
+//  Created by Robin Wu on 11/27/15.
+//  Copyright © 2015 roadie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelDetailController : UIViewController
+
+@end
