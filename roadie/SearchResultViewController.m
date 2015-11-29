3c9e6c6f6991cb9c33cfae69db80e7b639a31455
Xin Suo <xsuo@groupon.com>
2015-11-29 15:59:09 -0800

    deselect on select

--- roadie/SearchResultViewController.m
+++ roadie/SearchResultViewController.m
@@ -60,4 +60,9 @@
     
     return cell;
 }
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
